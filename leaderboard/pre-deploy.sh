49de1bd4364b150a831f4ebbe1477ef195e5b479
rithviknishad <mail@rithviknishad.dev>
2024-03-18 16:11:13 +0530

    fix incorrect path

--- leaderboard/pre-deploy.sh
+++ leaderboard/pre-deploy.sh
@@ -6,6 +6,6 @@ git init
 git remote add --mirror=fetch upstream ${LEADERBOARD_REPO}
 git pull upstream main
 
-mkdir -p contributors data/github data/slack
+mkdir -p ../contributors ../data/github ../data/slack
 
 ln -s ../ ./data-repo
